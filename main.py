042a057ea9a2f4728ce5a14d7c05ae009c0fa824
Susanelizabetth <castillosusana55@gmail.com>
2022-11-06 20:20:28 -0600

    my frist proyect

---
+++ main.py
@@ -0,0 +1,8 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+
+driver = webdriver.Chrome('C:/chromedriver.exe')
+driver.maximize_window()
+driver.delete_all_cookies()
+driver.get('https://www.amazon.com/')
